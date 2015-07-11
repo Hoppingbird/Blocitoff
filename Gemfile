c01aaadc684d535652f23da16ce246578cecf2df
Hoppingbird <hoppingbird@gmail.com>
2015-07-11 14:53:48 -0400

    seeddata

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'bootstrap-sass'
 
 gem 'devise'
 
+gem 'faker'
+
 gem 'figaro', '1.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
@@ -57,14 +59,7 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
 
-# Use Unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+
+# create items
+ 20.times do
+     Item.create!(
+     name: Faker::Lorem.sentence,
+     completed: "false"
+ )
+ end
+
+ user = User.new(
+   email:    'member10@example.com',
+   password: 'helloworld'
+ )
+ user.skip_confirmation!
+ user.save!
+
+ user = User.new(
+   email:    'member9@example.com',
+   password: 'helloworld'
+ )
+ user.skip_confirmation!
+ user.save!
+
+ user = User.new(
+   email:    'member8@example.com',
+   password: 'helloworld'
+ )
+ user.skip_confirmation!
+ user.save!
+
+ user = User.new(
+   email:    'member7@example.com',
+   password: 'helloworld'
+ )
+ user.skip_confirmation!
+ user.save!
+
+user = User.new(
+   email:    'member6@example.com',
+   password: 'helloworld'
+ )
+ user.skip_confirmation!
+ user.save!
+
+user = User.all
+item = Item.all
+puts "Seed finished"
+puts "#{User.count} users"
+puts "#{Item.count} items"
+
